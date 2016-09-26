cacd3378bdf03afda70b741661e9ceb2ba0d1cd2
a1422020484 <1422020484@qq.com>
2016-09-26 10:50:31 +0800

    facade

---
+++ src/facade/Client.java
@@ -0,0 +1,16 @@
+package facade;
+
+/**
+ * @author yang
+ * 怎么都感觉这个设计模式没什么意思
+ */
+public class Client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FacadeMethod fb = new FacadeMethod();
+		fb.openFile();
+		
+	}
+
+}
